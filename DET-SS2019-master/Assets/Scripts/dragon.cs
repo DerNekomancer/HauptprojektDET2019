ed8f3044c6ca6e09402f545cdb41e9c564787451
David <david.blomeke@tu-dortmund.de>
2019-05-07 16:23:19 +0200

    Added dragon

---
+++ DET-SS2019-master/Assets/Scripts/dragon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragon : MonoBehaviour
+{
+    public Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.SetBool("Fly", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
