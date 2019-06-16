fd09b36b7bb5b5f6c0153e4ee116844b8adae8cb
DerNekomancer <david.bloemeke@udo.edu>
2019-06-16 20:02:21 +0200

    added an additional character, optimized ai a little, working on particle collider for Joe's flamebreath

---
+++ Hauptprojekt/DET-SS2019-master/Assets/Scripts/dragon.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class dragon : MonoBehaviour
+{
+    public Transform Player;
+    public Animator anim;
+    float moveSpeed = 0.2f;
+    int timePassed = 0;
+    public GameObject flamethrow;
+    public GameObject head;
+    int counter = 0;
+    int waitTime = 2;
+    GameObject instantiatedFlamebreath;
+    int maxCounter = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       // anim.SetBool("Fly", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, Player.position) < 20)
+        {
+            Vector3 headvector = head.transform.position;
+            transform.LookAt(Player);
+            if(anim.GetBool("isIdle") == false)             //flying
+            {
+                if (Vector3.Distance(transform.position, Player.position) < 6)
+                {
+                    anim.SetBool("flyAttack", true);
+                    if (anim.GetBool("flyAttack") == true)
+                    {
+                        if (instantiatedFlamebreath == null)
+                        {
+                            
+                            instantiatedFlamebreath = Instantiate(flamethrow, headvector, transform.rotation, head.transform);
+                            Debug.Log("instantiated flamebreath");
+                            counter++;
+                            Debug.Log("counter: " + counter);
+                        }
+                    }
+                    
+                    if (Input.GetKeyDown(KeyCode.M))
+                    {
+                        Destroy(instantiatedFlamebreath);
+                    }
+ 
+                }
+                if (Vector3.Distance(transform.position, Player.position) > 10)
+                {
+                 
+                    if (Input.GetKeyDown(KeyCode.M))
+                    {
+                        Destroy(instantiatedFlamebreath);
+                    }
+                    
+                    anim.SetBool("flyAttack", false);
+                    if (counter >= 1)
+                    {
+                        if (instantiatedFlamebreath != null) {  Destroy(instantiatedFlamebreath); }
+                    }
+                   
+
+                }
+                if (Vector3.Distance(transform.position, Player.position) <= 15 && Vector3.Distance(transform.position, Player.position) > 7)
+                {
+                    transform.Translate(0f, 0f, 5f * moveSpeed * Time.deltaTime);
+                }
+
+            }
+            else                                             //on the ground
+            {
+                if(Vector3.Distance(transform.position, Player.position) < 8)
+                {
+                    anim.SetBool("standAttack", true);
+                    anim.SetBool("walking", false);
+                }
+                if (Vector3.Distance(transform.position, Player.position) <= 15 && Vector3.Distance(transform.position, Player.position) > 7)
+                {
+                    anim.SetBool("walking", true);
+                    transform.Translate(0f, 0f, 5f * moveSpeed * Time.deltaTime);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                anim.SetBool("isIdle", false);
+            }
+            else if (Input.GetKeyDown(KeyCode.O))
+            {
+                anim.SetBool("isIdle", true);
+            }
+        }
+    }
+}
